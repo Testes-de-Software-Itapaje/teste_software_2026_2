683fc5a13698ef02a288b7c74f41ba14f366ed9f
Italo <italolinhares@ufc.br>
2026-08-28 13:54:52 -0300

    Atualizando o código do github

--- tests/test_produto.py
+++ tests/test_produto.py
@@ -1,8 +1,9 @@
-from produto import Produto
+from entity.produto import Produto
 import pytest
-from excecoes import NomeInvalidoError
+from exceptions.excecoes import NomeInvalidoError
 
 def test_criar_produto_com_sucesso():
+    """Garante que o Produto seja criado com dados válidos."""
     cafe = Produto("cafe", 18, 50, 3, 1234567890, "alimenticio", 250)
     assert cafe._nome == "cafe"
     assert cafe._preco == 18 and cafe._quant_estoque == 50 and \
@@ -10,5 +11,6 @@ def test_criar_produto_com_sucesso():
         cafe._categoria == "alimenticio" and cafe._peso == 250
 
 def test_criar_produto_sem_nome():
+    """Garante que o Produto rejeite um nome vazio."""
     with pytest.raises(NomeInvalidoError):
         Produto("", 18, 50, 3, 1234567890, "alimenticio", 250)
